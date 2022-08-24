d425429a080306ec317dec2f2274206db3b5dd85
Kato Masato <masato.kato0604@gmail.com>
2022-08-24 14:48:35 +0900

    add loop func

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-    let mut input = String::new();
-
-    io::stdin().read_line(&mut input)?;
-
-    println!("{}", input.trim());
+    lsh_loop();
     Ok(())
+}
+
+fn lsh_loop() {
+    let mut line = String::new();
+    let mut args = String::new();
+    let status: i32;
+    loop {
+        println!("> ");
+        line = lsh_read_line();
+        // args = lsh_split_line(line);
+        // status = lsh_execute(args);
+        println!("{}", line);
+    }
+}
+
+fn lsh_read_line() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+    input
 }
