5f2de4f0481de10ea0f33c06a0f3605fd01dfd49
Kato Masato <masato.kato0604@gmail.com>
2022-08-26 21:26:55 +0900

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-use std::{io, str::SplitWhitespace};
+use std::{io, fmt, str::SplitWhitespace};
+use std::error;
 use fork::{fork, Fork};
+use nix::sys::wait::*;
+
+// type Result<T> = std::result::Result<T, LshError>;
+
+#[derive(Debug, Clone)]
+struct LshError;
+
+impl fmt::Display for LshError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid first item to double")
+    }
+}
+
+impl error::Error for LshError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
+}
+
+pub enum Status {
+    Success,
+    Exit
+}
 
 fn main() -> io::Result<()> {
     lsh_loop();
@@ -7,8 +31,7 @@ fn main() -> io::Result<()> {
 }
 
 fn lsh_loop() {
-    let mut line = String::new();
-    let status: i32;
+    let mut line = String::new(); let status: i32;
     loop {
         println!("> ");
         line = lsh_read_line();
@@ -28,16 +51,20 @@ fn lsh_split_line(line: &str) -> SplitWhitespace {
     line.split_whitespace()
 }
 
-fn lsh_launch(args: SplitWhitespace) -> i32 {
-    let status: i32;
+fn lsh_launch(args: SplitWhitespace) -> Result<Status, LshError> {
     let mut pid = fork();
     match pid {
         Ok(Fork::Parent(child)) => {
-            let wpid_result = waitpid(child)
+            let wpid_result = waitpid(child, None);
+            match wpid_result {
+                Ok(WaitStatus::Exited(_, _)) => Ok(Status::Success),
+                Ok(WaitStatus::Signaled(_, _, _)) => Ok(Status::Success),
+                Err(err) => LshError
+            }
         }
         Ok(Fork::Child) => {
             println!("child");
         }
-        Err(_) => println!("err");
+        Err(err) => LshError
     }
 }
